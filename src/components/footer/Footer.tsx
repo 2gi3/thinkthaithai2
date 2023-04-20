3c4159f7f6fb48d0c112faf30704615eb94f5ad7
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2023-04-20 17:02:22 +0100

    Footer card completed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,28 @@
+import Image from "next/image";
 import Contacts from "../Contacts/Contacts";
 import styles from "./Footer.module.scss";
+import { useTranslation } from "next-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation("common");
   return (
     <footer>
       <Contacts />
       <div className={styles.credits}>
-        <p></p>
-        <h3></h3>
-        <p></p>
-        <p></p>
+        <div className={styles.image}>
+          <Image
+            alt="Giuseppe Ippolito"
+            src="/popOutProfile.webp"
+            width={104}
+            height={119}
+          />
+        </div>
+        <div className={styles.info}>
+          <p>{t("Website by")}</p>
+          <h3>{t("Giuseppe Ippolito")}</h3>
+          <p>{t("Web-Developer")}</p>
+          <p>{t("London")}</p>
+        </div>
       </div>
     </footer>
   );
