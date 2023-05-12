06d06d874285dc0c273ef996bb2d62b0c8bf8386
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-05-12 04:27:45 +0100

    Documentation updated

--- src/pages/api/calendar/index.ts
+++ src/pages/api/calendar/index.ts
@@ -3,3 +3,9 @@ import {google} from 'googleapis'
 const {OAuth2} = google.auth 
 
 const OAuthClient = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET)
+
+OAuthClient.setCredentials({refresh_token: process.env.OAUTH_REFRESH_TOKEN})
+
+const calendar = google.calendar({version:'v3', auth: OAuthClient})
+
+// min 18:00
