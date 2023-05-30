41961b3de4c8a2cad3ed619fcc8081ff62713267
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-05-30 21:20:41 +0100

    test

--- src/pages/teacher/courses/[id].tsx
+++ src/pages/teacher/courses/[id].tsx
@@ -1,220 +1,188 @@
-import { DatabaseCourse, databaseStudent } from '@/types';
-import { useSession } from 'next-auth/react'
-import styles from '@/pages/courses/courses.module.scss'
-import Link from "next/link";
-import { GetStaticPropsContext } from 'next';
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import { useEffect, useState } from 'react';
-import { useRouter } from "next/router";
-import CourseIntroduction from '@/components/courses/CourseIntroduction';
-import Lesson from '@/components/courses/Lesson';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
-import { updateStudent } from '@/redux/slices/studentSlice';
+// import { DatabaseCourse, databaseStudent } from '@/types';
+// import { useSession } from 'next-auth/react'
+// import styles from '@/pages/courses/courses.module.scss'
+// import Link from "next/link";
+// import { GetStaticPropsContext } from 'next';
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+// import { useEffect, useState } from 'react';
+// import { useRouter } from "next/router";
+// import CourseIntroduction from '@/components/courses/CourseIntroduction';
+// import Lesson from '@/components/courses/Lesson';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { RootState } from '@/redux/store';
+// import { updateStudent } from '@/redux/slices/studentSlice';
 
 
-export default function About({ course }: { course: DatabaseCourse }) {
+// export default function About({ course }: { course: DatabaseCourse }) {
 
 
-    const dispatch = useDispatch();
-    const studentDataRedux = useSelector(
-        (state: RootState) => state.student
-    );
-    const router = useRouter();
-
-
-
-
-    const { data } = useSession();
-    // const [studentData, setStudentData] = useState<databaseStudent | null>(null);
-    const [startCourseCompleted, setStartCourseCompleted] = useState(false);
-    const [currentLesson, setCurrentLesson] = useState(0)
-    // const [completedLessons, setCompletedLessons] = useState<string []>()
-
-    // const handleLoadStudentData = () => {
-    //     const savedDataString = localStorage.getItem('databaseStudent');
-    //     const parsedData: databaseStudent = savedDataString ? JSON.parse(savedDataString) : null;
-    //     setStudentData(parsedData);
-    //     dispatch(updateStudent(parsedData));
-    // };
-
-    const handleStartCourseCompleted = (completed: boolean) => {
-        setStartCourseCompleted(completed);
-    };
-
-    const completedLessons = studentDataRedux?.startedCourses?.[course._id]
-    const completeLesson = async (title: string) => {
-
-        if (completedLessons?.includes(title)) {
-            setCurrentLesson(prev => prev + 1)
-        } else {
-            try {
-
-                await fetch("/api/students", {
-                    method: "PATCH",
-                    headers: {
-                        "content-type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        courseId: course._id,
-                        lessonTitle: title,
-                        studentEmail: data?.user?.email,
-                    }),
-                }).then(res => res.json())
-                // .then(json => console.log(json))
-
-            } catch (error) {
-                console.log(error);
-            } finally {
-                const response: databaseStudent = await fetch(`/api/students?searchBy=email&value=${data?.user?.email}`).then((res) => res.json());
-                localStorage.setItem('databaseStudent', JSON.stringify(response));
-                setCurrentLesson(prev => prev + 1)
-            }
-        }
-    }
-
-
-
-    const lesson = course.lessons[currentLesson]
-    console.log(currentLesson)
-    console.log(course.lessons.length
-    )
-    const incompleteLessonIndex = course.lessons.findIndex(
-        (lesson) => !completedLessons?.includes(lesson.title)
-    );
-
-    useEffect(() => {
-        // const handleLoadStudentData = () => {
-        //     const savedDataString = localStorage.getItem('databaseStudent');
-        //     const parsedData: databaseStudent = savedDataString ? JSON.parse(savedDataString) : null;
-        //     setStudentData(parsedData);
-        //     dispatch(updateStudent(parsedData));
-        //     console.log(studentDataRedux)
-        // };
-        const handleLoadStudentData = () => {
-            const savedDataString = localStorage.getItem('databaseStudent');
-            const parsedData: databaseStudent = savedDataString ? JSON.parse(savedDataString) : null;
-            // setStudentData(parsedData);
-            dispatch(updateStudent(parsedData));
-        };
-
-        handleLoadStudentData()
-        // setCurrentLesson(incompleteLessonIndex !== -1 ? incompleteLessonIndex : 0)
-    }, [startCourseCompleted, currentLesson, dispatch])
-
-    useEffect(() => {
-        setCurrentLesson(incompleteLessonIndex !== -1 ? incompleteLessonIndex : 0)
-
-    }, [incompleteLessonIndex])
-
-    const startedCourses = studentDataRedux?.startedCourses
-
-
-    if (startCourseCompleted || startedCourses?.hasOwnProperty(course._id)) {
-
-        // Memo to create CourseCompleted page
-        // if ( completedLessons.length => course.lessons.length ) return <CourseCompleted />
-
-        return (
-            <div className={styles.container}>
-                <header>
-                    <h1>{course.title}</h1>
-                    {/* <p>Progress {course.lessons.length}</p> */}
-                    <div className={styles.progressContainer}>
-                        {course.lessons.map((lesson, i: number) => (
-                            <button key={i}
-                                onClick={() => { setCurrentLesson(i) }}
-                                className={completedLessons?.includes(lesson.title) ? styles.progress : styles.toLearn}>
-                                <span>{lesson.title}</span>
-                                {currentLesson === i + 1 ? <div className={styles.current} ></div> : null}
-                            </button>
-
-                        ))}
-
-                    </div>
-                </header>
-                <main>
-                    {currentLesson >= course.lessons.length ?
-                        <h2>Course completed</h2>
-                        : <Lesson lesson={lesson} />
-                    }
-                </main>
-                <footer>
-                    <button className={styles.previous}
-                        onClick={() => {
-                            if (currentLesson === 0) {
-                                router.push('/courses')
-                            } else { setCurrentLesson(x => x - 1) }
-                        }}>
-                        <FaArrowLeft />
-                    </button>
-
-                    {
-                        currentLesson === course.lessons.length ?
-                            <button onClick={() => {
-                                router.push('/courses')
-                            }}>Start a new course</button>
-                            : <button onClick={() => {
-                                completeLesson(lesson.title)
-                            }}>
-                                Lesson completed &nbsp; <FaArrowRight />
-                            </button>
-
-                    }
-
-                    {/* <button onClick={() => {
-                        completeLesson(lesson.title)
-                    }}>Lesson completed &nbsp; <FaArrowRight /></button> */}
-                </footer>
-
-                {/* {course.lessons.map((lesson, i: number) => (
-                    <div key={i} className={styles.container}> */}
-                {/* <Lesson lesson={lesson} /> */}
-                {/* </div>
-                ))} */}
-            </div>
-
-        )
-    } else {
-        return (
-            <CourseIntroduction
-                course={course}
-                setStartCourseCompleted={handleStartCourseCompleted}
-            />
-
-        )
-    }
-
-
-}
-
-
-export async function getStaticPaths() {
-
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses`, {
-        method: "GET",
-    });
-    const courses: DatabaseCourse[] = await res.json();
+//     const dispatch = useDispatch();
+//     const studentDataRedux = useSelector(
+//         (state: RootState) => state.student
+//     );
+//     const router = useRouter();
 
 
 
 
-    const paths = courses.map((course) => ({
-        params: { id: course._id },
-    }));
+//     const { data } = useSession();
+//     const [startCourseCompleted, setStartCourseCompleted] = useState(false);
+//     const [currentLesson, setCurrentLesson] = useState(0)
+   
+//     const handleStartCourseCompleted = (completed: boolean) => {
+//         setStartCourseCompleted(completed);
+//     };
 
-    return {
-        paths,
-        fallback: false,
-    };
-}
+//     const completedLessons = studentDataRedux?.startedCourses?.[course._id]
+//     const completeLesson = async (title: string) => {
 
-export async function getStaticProps(context: GetStaticPropsContext) {
-    const id = context.params!.id
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses?searchBy=id&value=${id}`, {
-        method: "GET",
-    });
-    const course: DatabaseCourse = await res.json();
+//         if (completedLessons?.includes(title)) {
+//             setCurrentLesson(prev => prev + 1)
+//         } else {
+//             try {
 
-    return { props: { course }, revalidate: 60 };
-};
+//                 await fetch("/api/students", {
+//                     method: "PATCH",
+//                     headers: {
+//                         "content-type": "application/json",
+//                     },
+//                     body: JSON.stringify({
+//                         courseId: course._id,
+//                         lessonTitle: title,
+//                         studentEmail: data?.user?.email,
+//                     }),
+//                 }).then(res => res.json())
+
+//             } catch (error) {
+//                 console.log(error);
+//             } finally {
+//                 const response: databaseStudent = await fetch(`/api/students?searchBy=email&value=${data?.user?.email}`).then((res) => res.json());
+//                 localStorage.setItem('databaseStudent', JSON.stringify(response));
+//                 setCurrentLesson(prev => prev + 1)
+//             }
+//         }
+//     }
+
+
+
+//     const lesson = course.lessons[currentLesson]
+//     console.log(currentLesson)
+//     console.log(course.lessons.length
+//     )
+//     const incompleteLessonIndex = course.lessons.findIndex(
+//         (lesson) => !completedLessons?.includes(lesson.title)
+//     );
+
+//     useEffect(() => {
+//         const handleLoadStudentData = () => {
+//             const savedDataString = localStorage.getItem('databaseStudent');
+//             const parsedData: databaseStudent = savedDataString ? JSON.parse(savedDataString) : null;
+//             dispatch(updateStudent(parsedData));
+//         };
+
+//         handleLoadStudentData()
+//     }, [startCourseCompleted, currentLesson, dispatch])
+
+//     useEffect(() => {
+//         setCurrentLesson(incompleteLessonIndex !== -1 ? incompleteLessonIndex : 0)
+
+//     }, [incompleteLessonIndex])
+
+//     const startedCourses = studentDataRedux?.startedCourses
+
+
+//     if (startCourseCompleted || startedCourses?.hasOwnProperty(course._id)) {
+
+//         return (
+//             <div className={styles.container}>
+//                 <header>
+//                     <h1>{course.title}</h1>
+//                     <div className={styles.progressContainer}>
+//                         {course.lessons.map((lesson, i: number) => (
+//                             <button key={i}
+//                                 onClick={() => { setCurrentLesson(i) }}
+//                                 className={completedLessons?.includes(lesson.title) ? styles.progress : styles.toLearn}>
+//                                 <span>{lesson.title}</span>
+//                                 {currentLesson === i + 1 ? <div className={styles.current} ></div> : null}
+//                             </button>
+
+//                         ))}
+
+//                     </div>
+//                 </header>
+//                 <main>
+//                     {currentLesson >= course.lessons.length ?
+//                         <h2>Course completed</h2>
+//                         : <Lesson lesson={lesson} />
+//                     }
+//                 </main>
+//                 <footer>
+//                     <button className={styles.previous}
+//                         onClick={() => {
+//                             if (currentLesson === 0) {
+//                                 router.push('/courses')
+//                             } else { setCurrentLesson(x => x - 1) }
+//                         }}>
+//                         <FaArrowLeft />
+//                     </button>
+
+//                     {
+//                         currentLesson === course.lessons.length ?
+//                             <button onClick={() => {
+//                                 router.push('/courses')
+//                             }}>Start a new course</button>
+//                             : <button onClick={() => {
+//                                 completeLesson(lesson.title)
+//                             }}>
+//                                 Lesson completed &nbsp; <FaArrowRight />
+//                             </button>
+
+//                     }
+
+//                 </footer>
+//             </div>
+
+//         )
+//     } else {
+//         return (
+//             <CourseIntroduction
+//                 course={course}
+//                 setStartCourseCompleted={handleStartCourseCompleted}
+//             />
+
+//         )
+//     }
+
+
+// }
+
+
+// export async function getStaticPaths() {
+
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses`, {
+//         method: "GET",
+//     });
+//     const courses: DatabaseCourse[] = await res.json();
+
+
+
+
+//     const paths = courses.map((course) => ({
+//         params: { id: course._id },
+//     }));
+
+//     return {
+//         paths,
+//         fallback: false,
+//     };
+// }
+
+// export async function getStaticProps(context: GetStaticPropsContext) {
+//     const id = context.params!.id
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses?searchBy=id&value=${id}`, {
+//         method: "GET",
+//     });
+//     const course: DatabaseCourse = await res.json();
+
+//     return { props: { course }, revalidate: 60 };
+// };
--- src/pages/teacher/courses/index.tsx
+++ src/pages/teacher/courses/index.tsx
@@ -1,46 +1,46 @@
-import { DatabaseCourse } from '@/types';
-import styles from '../../courses/courses.module.scss'
-import Link from "next/link";
+// import { DatabaseCourse } from '@/types';
+// import styles from '../../courses/courses.module.scss'
+// import Link from "next/link";
 
-export const getStaticProps = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses`, {
-        method: "GET",
-    });
-    const courses: DatabaseCourse[] = await res.json();
+// export const getStaticProps = async () => {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses`, {
+//         method: "GET",
+//     });
+//     const courses: DatabaseCourse[] = await res.json();
 
-    return { props: { courses }, revalidate: 60 };
-};
+//     return { props: { courses }, revalidate: 60 };
+// };
 
-export default function TeacherCourses({ courses }: { courses: DatabaseCourse[] }) {
-    return (
-        <div className={styles.container}>
-            <header>
-                <h1>
-                    The&nbsp;best&nbsp;time to&nbsp;start&nbsp;learning is&nbsp;Now!
-                </h1>
-            </header>
-            <main>
-                <div>
-                    <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/teacher/courses/create`}>
-                        Create new course
-                    </Link>
-                </div>
-                {courses.map((course: DatabaseCourse) => (
-                    <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/teacher/courses/${course._id}`} key={course._id}>
-                        <div className={styles.course}>
-                            <h3>
-                                {course.title}
-                            </h3>
-                            <p className={styles.price}>{course.status}</p>
-                            <p>{course.description}</p>
-                            <div className={styles.footer}>
-                                <p>Level: <span>{course.level}</span></p>
-                                <p>commitment: <span>15 minutes/day, 1 month</span></p>
-                            </div>
-                        </div>
-                    </Link>
-                ))}
-            </main>
-        </div>
-    );
-}
+// export default function TeacherCourses({ courses }: { courses: DatabaseCourse[] }) {
+//     return (
+//         <div className={styles.container}>
+//             <header>
+//                 <h1>
+//                     The&nbsp;best&nbsp;time to&nbsp;start&nbsp;learning is&nbsp;Now!
+//                 </h1>
+//             </header>
+//             <main>
+//                 <div>
+//                     <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/teacher/courses/create`}>
+//                         Create new course
+//                     </Link>
+//                 </div>
+//                 {courses.map((course: DatabaseCourse) => (
+//                     <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/teacher/courses/${course._id}`} key={course._id}>
+//                         <div className={styles.course}>
+//                             <h3>
+//                                 {course.title}
+//                             </h3>
+//                             <p className={styles.price}>{course.status}</p>
+//                             <p>{course.description}</p>
+//                             <div className={styles.footer}>
+//                                 <p>Level: <span>{course.level}</span></p>
+//                                 <p>commitment: <span>15 minutes/day, 1 month</span></p>
+//                             </div>
+//                         </div>
+//                     </Link>
+//                 ))}
+//             </main>
+//         </div>
+//     );
+// }
