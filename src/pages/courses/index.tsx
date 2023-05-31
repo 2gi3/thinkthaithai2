71eff61ed88b605a7949fe5e6940eda054e21b87
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-05-31 10:54:20 +0100

    Feedback and courses updated to use the production API

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -5,80 +5,79 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 
 export default function About(
-    // { courses }: { courses: DatabaseCourse[] }
+    { courses }: { courses: DatabaseCourse[] }
 ) {
 
-    //   const dispatch = useDispatch();
-    //   const studentData = useSelector(
-    //     (state: RootState) => state.student
-    //   );
+    const dispatch = useDispatch();
+    const studentData = useSelector(
+        (state: RootState) => state.student
+    );
 
-    //   const renderProgress = (course: DatabaseCourse) => {
+    const renderProgress = (course: DatabaseCourse) => {
 
-    //     const courseId = course._id;
-    //     const hasStartedCourse = studentData.startedCourses?.hasOwnProperty(courseId);
-    //     const startedCourseLength = studentData.startedCourses?.[courseId]?.length;
-    //     const lessonCount = course.lessons.length
+        const courseId = course._id;
+        const hasStartedCourse = studentData.startedCourses?.hasOwnProperty(courseId);
+        const startedCourseLength = studentData.startedCourses?.[courseId]?.length;
+        const lessonCount = course.lessons.length
 
-    //     let progressElement;
-    //     if (hasStartedCourse) {
-    //       if (startedCourseLength && startedCourseLength >= lessonCount) {
-    //         progressElement = (
-    //           <p className={styles.completed}>
-    //             <span>Completed</span>
-    //           </p>
-    //         );
-    //       } else {
-    //         const progressPercentage = Math.ceil((startedCourseLength! / lessonCount) * 100);
-    //         progressElement = (
-    //           <p className={styles.started}>
-    //             <span>Progress</span> {progressPercentage}%
-    //           </p>
-    //         );
-    //       }
-    //     } else {
-    //       progressElement = <p className={styles.price}>{course.status.charAt(0).toUpperCase() + course.status.slice(1).toLowerCase()}</p>;
-    //     }
+        let progressElement;
+        if (hasStartedCourse) {
+            if (startedCourseLength && startedCourseLength >= lessonCount) {
+                progressElement = (
+                    <p className={styles.completed}>
+                        <span>Completed</span>
+                    </p>
+                );
+            } else {
+                const progressPercentage = Math.ceil((startedCourseLength! / lessonCount) * 100);
+                progressElement = (
+                    <p className={styles.started}>
+                        <span>Progress</span> {progressPercentage}%
+                    </p>
+                );
+            }
+        } else {
+            progressElement = <p className={styles.price}>{course.status.charAt(0).toUpperCase() + course.status.slice(1).toLowerCase()}</p>;
+        }
 
-    //     return progressElement
+        return progressElement
 
-    //   }
+    }
 
     return (
-        <h1>hello</h1>
-        //     <div className={styles.container}>
-        //       <header>
-        //         <h1>
-        //           The&nbsp;best&nbsp;time to&nbsp;start&nbsp;learning is&nbsp;Now!
-        //         </h1>
-        //       </header>
-        //       <main>
-        //         {courses.map((course: DatabaseCourse) => (
-        //           <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/courses/${course._id}`} key={course._id}>
-        //             <div className={styles.course}>
-        //               <h3>
-        //                 {course.title}
-        //               </h3>
-        //               {renderProgress(course)}
-        //               <p>{course.description}</p>
-        //               <div className={styles.footer}>
-        //                 <p>Level: <span>{course.level}</span></p>
-        //               </div>
-        //             </div>
-        //           </Link>
-        //         ))}
-        //       </main>
-        //     </div>
+        <div className={styles.container}>
+            <header>
+                <h1>
+                    The&nbsp;best&nbsp;time to&nbsp;start&nbsp;learning is&nbsp;Now!
+                </h1>
+            </header>
+            <main>
+                {courses.map((course: DatabaseCourse) => (
+                    <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/courses/${course._id}`} key={course._id}>
+                        <div className={styles.course}>
+                            <h3>
+                                {course.title}
+                            </h3>
+                            {renderProgress(course)}
+                            <p>{course.description}</p>
+                            <div className={styles.footer}>
+                                <p>Level: <span>{course.level}</span></p>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </main>
+        </div>
     );
 }
 
 
 
-// export const getStaticProps = async () => {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses`, {
-//     method: "GET",
-//   });
-//   const courses: DatabaseCourse[] = await res.json();
+export const getStaticProps = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses`, {
+        method: "GET",
+    });
+    const courses: DatabaseCourse[] = await res.json();
 
-//   return { props: { courses }, revalidate: 60 };
-// };
+    return { props: { courses }, revalidate: 60 };
+};
--- src/pages/teacher/courses/index.tsx
+++ src/pages/teacher/courses/index.tsx
@@ -1,49 +1,48 @@
-// import { DatabaseCourse } from '@/types';
-// import styles from '../../courses/courses.module.scss'
-// import Link from "next/link";
+import { DatabaseCourse } from '@/types';
+import styles from '../../courses/courses.module.scss'
+import Link from "next/link";
 
-// export const getStaticProps = async () => {
-//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses`, {
-//         method: "GET",
-//     });
-//     const courses: DatabaseCourse[] = await res.json();
+export const getStaticProps = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/courses`, {
+        method: "GET",
+    });
+    const courses: DatabaseCourse[] = await res.json();
 
-//     return { props: { courses }, revalidate: 60 };
-// };
+    return { props: { courses }, revalidate: 60 };
+};
 
 export default function TeacherCourses(
-    // { courses }: { courses: DatabaseCourse[] }
+    { courses }: { courses: DatabaseCourse[] }
 ) {
     return (
-        <h1>hello</h1>
-        //         <div className={styles.container}>
-        //             <header>
-        //                 <h1>
-        //                     The&nbsp;best&nbsp;time to&nbsp;start&nbsp;learning is&nbsp;Now!
-        //                 </h1>
-        //             </header>
-        //             <main>
-        //                 <div>
-        //                     <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/teacher/courses/create`}>
-        //                         Create new course
-        //                     </Link>
-        //                 </div>
-        //                 {courses.map((course: DatabaseCourse) => (
-        //                     <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/teacher/courses/${course._id}`} key={course._id}>
-        //                         <div className={styles.course}>
-        //                             <h3>
-        //                                 {course.title}
-        //                             </h3>
-        //                             <p className={styles.price}>{course.status}</p>
-        //                             <p>{course.description}</p>
-        //                             <div className={styles.footer}>
-        //                                 <p>Level: <span>{course.level}</span></p>
-        //                                 <p>commitment: <span>15 minutes/day, 1 month</span></p>
-        //                             </div>
-        //                         </div>
-        //                     </Link>
-        //                 ))}
-        //             </main>
-        //         </div>
+        <div className={styles.container}>
+            <header>
+                <h1>
+                    The&nbsp;best&nbsp;time to&nbsp;start&nbsp;learning is&nbsp;Now!
+                </h1>
+            </header>
+            <main>
+                <div>
+                    <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/teacher/courses/create`}>
+                        Create new course
+                    </Link>
+                </div>
+                {courses.map((course: DatabaseCourse) => (
+                    <Link href={`${process.env.NEXT_PUBLIC_BASIC_URL}/teacher/courses/${course._id}`} key={course._id}>
+                        <div className={styles.course}>
+                            <h3>
+                                {course.title}
+                            </h3>
+                            <p className={styles.price}>{course.status}</p>
+                            <p>{course.description}</p>
+                            <div className={styles.footer}>
+                                <p>Level: <span>{course.level}</span></p>
+                                <p>commitment: <span>15 minutes/day, 1 month</span></p>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </main>
+        </div>
     );
 }
