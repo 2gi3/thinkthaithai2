67317be94b5c59633fb87beceda566b8e72650dd
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-06-20 00:21:52 +0100

    log in page styled

--- src/pages/access/index.tsx
+++ src/pages/access/index.tsx
@@ -5,12 +5,16 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { BsGoogle, BsFacebook } from "react-icons/bs";
 import { FormEvent, useState } from "react";
 import Spinner from "@/components/Spinner";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 export default function About() {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // Disable SSL certificate checks --USE ONLY ON DEVELOPMENT ENVIRONMENT--
   const { data: session, status } = useSession();
   const [email, setEmail] = useState("");
   const { push } = useRouter();
+  const { t } = useTranslation("common");
+
   // SIGN IN ROUTE ${process.env.NEXT_PUBLIC_BASIC_URL}/api/auth/signin
 
   const handleLogOut = async () => {
@@ -45,16 +49,25 @@ export default function About() {
             autoFocus={true}
             onChange={(e) => setEmail(e.target.value)}
           ></input>
-          <button type="submit">Log in with Email</button>
+          <button type="submit">{t('Log in with Email')}</button>
         </form>
         <p>or</p>
         <button onClick={() => handleLogIn("google")}>
-          <BsGoogle /> Log in with Google
+          <BsGoogle /> {t('Log in with Google')}
         </button>
         <button onClick={() => handleLogIn("facebook")}>
-          <BsFacebook /> Log in with Facebook
+          <BsFacebook /> {t('Log in with Facebook')}
         </button>
       </div>
     );
   }
+}
+
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
 }
