31aa43990120322e0f0aa19762f8b257477719fb
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-08-13 17:25:53 +0100

    updated trial lesson to 50 minutes

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -67,7 +67,7 @@ function Calendar({ label, eventURL, email, name }: { label: string, eventURL: s
       <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
       <button onClick={handleScheduleClick}>{label}{
         label === 'Trial Lesson' && <><Price USD={5} />
-          <p>25 minutes</p>
+          <p>50 minutes</p>
           <p>No commitment</p></>
       }</button>
     </div>
