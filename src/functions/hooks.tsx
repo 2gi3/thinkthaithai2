aa0212355c608c819f869d4ab0217896c5199607
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-08-30 04:20:16 +0100

    Created forex API to fetch exchange rates from the server

--- src/functions/hooks.tsx
+++ src/functions/hooks.tsx
@@ -8,26 +8,17 @@ import { useDispatch } from "react-redux";
 export const useExchangeRate = (newRate: string, amount = 5) => {
   const [exchangeRate, setExchangeRate] = useState<number | null>(null);
 
-  // const fetchExchangeRate = async () => {
-  //   const URL = process.env.NEXT_PUBLIC_CURRENCY_EXCHANGE_URL;
-  //   console.log(URL);
-  //   try {
-  //     const raw = await fetch(URL!);
-  //     const { data } = await raw.json();
-  //     const newCurrencyValue = data.rates[newRate] * amount;
-  //     setExchangeRate(Number(newCurrencyValue.toFixed(2)));
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
   useEffect(() => {
     const fetchExchangeRate = async () => {
-      const URL = process.env.NEXT_PUBLIC_CURRENCY_EXCHANGE_URL;
+      const URL = process.env.NEXT_PUBLIC_BASIC_URL!;
       console.log(URL);
       try {
-        const raw = await fetch(URL!);
+        const raw = await fetch(`${URL}/api/forex`);
         const { data } = await raw.json();
+
+        console.log('b4')
+        console.log(data)
+        console.log('af')
         const newCurrencyValue = data.rates[newRate] * amount;
         setExchangeRate(Number(newCurrencyValue.toFixed(2)));
       } catch (error) {
---
+++ src/pages/api/forex/index.tsx
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from "next-auth/next"
+import { authOptions } from '../auth/[...nextauth]';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const session = await getServerSession(req, res, authOptions)
+
+        const isUserAuthorized = session?.user
+
+        if (isUserAuthorized) {
+            res.setHeader('Access-Control-Allow-Origin', process.env.NEXT_PUBLIC_BASIC_URL!);
+            res.setHeader('Access-Control-Allow-Methods', 'GET');
+            res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+
+            if (req.method === 'OPTIONS') {
+                res.status(200).end();
+                return;
+            }
+
+            const coinbaseResponse = await fetch('https://api.coinbase.com/v2/exchange-rates?currency=USD');
+
+            if (coinbaseResponse.ok) {
+                const data = await coinbaseResponse.json();
+                res.status(200).json(data);
+            } else {
+                res.status(coinbaseResponse.status).end(coinbaseResponse.statusText);
+            }
+        } else {
+            res.status(403).json({ error: 'Unauthorized access' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+};
+
+export default handler;
