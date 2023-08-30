822f924d9f42b675ebcad8f1d3d82de3edecb69b
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-08-30 14:17:36 +0100

    Updated the useExchangeRate hook to increase speed

--- src/functions/hooks.tsx
+++ src/functions/hooks.tsx
@@ -10,16 +10,18 @@ export const useExchangeRate = (newRate: string, amount = 5) => {
       const URL = process.env.NEXT_PUBLIC_BASIC_URL!;
       console.log(URL);
       try {
-        const raw = await fetch(`${URL}/api/forex`);
-        const { data } = await raw.json();
-        const newCurrencyValue = data.rates[newRate] * amount;
+
+        const raw = await fetch('https://api.exchangerate.host/latest?base=USD');
+        const { rates } = await raw.json();
+        const newCurrencyValue = rates[newRate] * amount;
         setExchangeRate(Number(newCurrencyValue.toFixed(2)));
+
       } catch (error) {
         console.error(error);
         try {
-          const raw = await fetch('https://api.exchangerate.host/latest?base=USD');
-          const { rates } = await raw.json();
-          const newCurrencyValue = rates[newRate] * amount;
+          const raw = await fetch(`${URL}/api/forex`);
+          const { data } = await raw.json();
+          const newCurrencyValue = data.rates[newRate] * amount;
           setExchangeRate(Number(newCurrencyValue.toFixed(2)));
         } catch (error) {
           console.error('Currencies updated to 30/08/2023');
