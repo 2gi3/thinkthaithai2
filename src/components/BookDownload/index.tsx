c3d0846eb7900cf7929ea7b1e73b5b8dfce53695
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2024-10-05 23:44:32 +0100

    Created audio files selector

--- src/components/BookDownload/index.tsx
+++ src/components/BookDownload/index.tsx
@@ -1,11 +1,13 @@
 import { FaTimes } from 'react-icons/fa'
 import styles from './BookDownload.module.scss'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { BookDownloadProps } from '@/types'
 
 const BookDownload: FC<BookDownloadProps> = ({
     onClose,
 }) => {
+    const [showAudioFiles, setShowAudioFiles] = useState(false);
+
     return (
         <div className={styles.BookDownloadContainer}>
             <button className={styles.closeButton}
@@ -22,8 +24,21 @@ const BookDownload: FC<BookDownloadProps> = ({
                 </a>
                 {/* <button className='secondaryButton'>Book</button> */}
                 <button className='secondaryButton'
-                    onClick={() => { console.log("audioFiles") }}>Audio files</button>
+                    onClick={() => setShowAudioFiles(!showAudioFiles)}>
+                    {showAudioFiles ? 'Hide Audio Files' : 'Audio files'}
+                </button>
             </div>
+            {showAudioFiles && (
+                <div className={styles.audioFilesList}>
+                    <ul>
+                        {Array.from({ length: 100 }, (_, i) => (
+                            <li key={i + 1}>
+                                <a href={`/audio/file_${i + 1}.mp3`} download>{`Audio File ${i + 1}`}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
